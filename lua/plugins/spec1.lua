a4ff26e56862670e21c891ae78711704432c00ab
Myeonghun Choi <mhchoi@uos.ac.kr>
2025-03-06 15:53:24 +0900

    quit 2 space-bar as esc in vi and add gitsigns

--- lua/plugins/spec1.lua
+++ lua/plugins/spec1.lua
@@ -29,22 +29,75 @@ return {
 	},
 
 	{
-		"airblade/vim-gitgutter",
-		lazy = false,
-		keys = {
-			{ "<leader>hp", "<Plug>(GitGutterPreviewHunk)", desc = "git preview"}
-		},
-		init = function()
-			-- set git hunk color
-			vim.cmd[[
-				highlight GitGutterAdd    guifg=#009900 ctermfg=Green
-				highlight GitGutterChange guifg=#bbbb00 ctermfg=Yellow
-				highlight GitGutterDelete guifg=#ff2222 ctermfg=Red
-				highlight clear SignColumn
-			]]
-			-- set floating window color
-			vim.api.nvim_set_hl(0, "FloatBorder", {bg="#3B4252", fg="#5E81AC"})
-			vim.api.nvim_set_hl(0, "NormalFloat", {bg="#3B4252"})
+--		"airblade/vim-gitgutter",
+--		lazy = false,
+--		keys = {
+--			{ "<leader>hp", "<Plug>(GitGutterPreviewHunk)", desc = "git preview"}
+--		},
+--		init = function()
+--			-- set git hunk color
+--			vim.cmd[[
+--				highlight GitGutterAdd    guifg=#009900 ctermfg=Green
+--				highlight GitGutterChange guifg=#bbbb00 ctermfg=Yellow
+--				highlight GitGutterDelete guifg=#ff2222 ctermfg=Red
+--				highlight clear SignColumn
+--			]]
+--			-- set floating window color
+--			vim.api.nvim_set_hl(0, "FloatBorder", {bg="#3B4252", fg="#5E81AC"})
+--			vim.api.nvim_set_hl(0, "NormalFloat", {bg="#3B4252"})
+--		end,
+		"lewis6991/gitsigns.nvim",
+		config = function()
+			require('gitsigns').setup {
+				signs = {
+				add          = { text = '+' },
+				change       = { text = '~' },
+				delete       = { text = '_' },
+				topdelete    = { text = '‾' },
+				changedelete = { text = '~~' },
+				untracked    = { text = '┆' },
+				},
+				signs_staged = {
+					add          = { text = '+' },
+					change       = { text = '~' },
+					delete       = { text = '_' },
+					topdelete    = { text = '‾' },
+					changedelete = { text = '~~' },
+					untracked    = { text = '┆' },
+				},
+				signs_staged_enable = true,
+				signcolumn = true,  -- Toggle with `:Gitsigns toggle_signs`
+				numhl      = false, -- Toggle with `:Gitsigns toggle_numhl`
+				linehl     = false, -- Toggle with `:Gitsigns toggle_linehl`
+				word_diff  = false, -- Toggle with `:Gitsigns toggle_word_diff`
+				watch_gitdir = {
+					follow_files = true
+				},
+				auto_attach = true,
+				attach_to_untracked = false,
+				current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
+				current_line_blame_opts = {
+					virt_text = true,
+					virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
+					delay = 1000,
+					ignore_whitespace = false,
+					virt_text_priority = 100,
+					use_focus = true,
+				},
+				current_line_blame_formatter = '<author>, <author_time:%R> - <summary>',
+				sign_priority = 6,
+				update_debounce = 100,
+				status_formatter = nil, -- Use default
+				max_file_length = 40000, -- Disable if file is longer than this (in lines)
+				preview_config = {
+					-- Options passed to nvim_open_win
+					border = 'single',
+					style = 'minimal',
+					relative = 'cursor',
+					row = 0,
+					col = 1
+				},
+			}
 		end,
 	},
 
